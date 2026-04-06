ee5200d544eb7b927d8959710dff4a63f822a5fc
rafaellucas30290 <orafaellucas5@gmail.com>
2026-04-06 16:02:23 -0300

    first commit

---
+++ src/main/java/com/projetoDevOps/ItemStockAPI/ItemStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.projetoDevOps.ItemStockAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoDevOps/ItemStockAPI/ItemStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoDevOps.ItemStockAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
